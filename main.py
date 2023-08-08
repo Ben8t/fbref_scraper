dcb5c174c44afea991291fb4b60c23292c4b482e
Benoit <benoit@kestra.io>
2023-08-08 14:59:39 +0200

    [feature] add fixture parsing

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from utils import *
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse fbref /matchs page")
     parser.add_argument("--date", help="The match date")
+    parser.add_argument("--page", help="matchs/ or fixtures/")
+    parser.add_argument("--fixture_url", help="fixture url")
     args = parser.parse_args()
 
     if not is_date_format(args.date):
@@ -17,19 +19,30 @@ if __name__ == "__main__":
     # date = "2023-01-03"
     date = args.date
     
+    if args.page == "matchs/":
 
-    url = f"https://fbref.com/en/matchs/{date}"
+        url = f"https://fbref.com/en/matchs/{date}"
 
-    response = requests.get(url)
-    tree = html.fromstring(response.content)
-    items = tree.xpath('//div[@class="table_wrapper tabbed"]')
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        items = tree.xpath('//div[@class="table_wrapper tabbed"]')
 
-    results = []
-    for item in items:
-        league = item.find("span").text.replace('"','').replace('>','')
-        table = etree.fromstring(etree.tostring(item)).xpath("//table")[0]
-        df = pd.read_html(etree.tostring(table))[0].assign(league=league, date=date)
-        results.append(df)
+        results = []
+        for item in items:
+            league = item.find("span").text.replace('"','').replace('>','')
+            table = etree.fromstring(etree.tostring(item)).xpath("//table")[0]
+            df = pd.read_html(etree.tostring(table))[0].assign(league=league, date=date)
+            results.append(df)
 
-    final_data = snake_case_column_names(pd.concat(results))
-    final_data.to_csv(f"data/matchs_{date.replace('-','')}.csv", index=False)
+        final_data = snake_case_column_names(pd.concat(results))
+        final_data.to_csv(f"data/matchs_{date.replace('-','')}.csv", index=False)
+
+    if args.page == "fixtures/" and args.fixture_url is not None:
+        url = args.fixture_url
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        league = url.split("/")[-1].replace("-Scores-and-Fixtures","").replace("-"," ")
+        table = etree.fromstring(etree.tostring(tree.xpath('//*[@class="table_wrapper tabbed"]')[0])).xpath("//table")[0]
+        final_data = pd.read_html(etree.tostring(table))[0].assign(league=league)
+        final_data = snake_case_column_names(final_data)
+        final_data.to_csv(f'data/fixtures_{league.lower().replace(" ","_")}.csv', index=False)
